f0193217b3b441ca65a809289603d2428dcdfa39
Emil Rofors <emirof@gmail.com>
2024-09-23 15:11:39 -0700

    Initial commit

---
+++ Assets/Kart/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        private KartControls inputActions;
+
+        private void OnEnable() {
+            inputActions = new KartControls();
+            inputActions.Enable();
+        }
+
+        private void OnDisable() {
+            inputActions.Disable();
+        }
+
+        public override InputData GenerateInput() {
+            var inputData = new InputData
+            {
+                Accelerate = inputActions.Kart.Accelerate.ReadValue<float>() > 0,
+                Brake = inputActions.Kart.Brake.ReadValue<float>() > 0,
+                TurnInput = inputActions.Kart.Turn.ReadValue<Vector2>().x
+            };
+            return inputData;
+        }
+    }
+}
